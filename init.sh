51c3d8a2dc5c44ec865385b238e09ecf5c1eae99
Benedict <shikifuchi@hotmail.com>
2023-09-10 21:46:31 +0800

    change browser

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo apt install build-essential git vim neovim python3 nodejs\
   lightdm slick-greeter i3 polybar rofi suckless-tools\
-  Iceweasel realvnc-vnc-server realvnc-vnc-viewer 
+  chromium realvnc-vnc-server realvnc-vnc-viewer 
 
   

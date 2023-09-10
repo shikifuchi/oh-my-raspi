db9b8cfe3d394b555462468c8d2a231cb18d741a
Benedict <shikifuchi@hotmail.com>
2023-09-10 23:08:12 +0800

    cockpit

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 sudo apt update --yes
 sudo apt install --yes build-essential git vim neovim python3 nodejs\
   lightdm slick-greeter i3 polybar rofi suckless-tools\
-  chromium realvnc-vnc-server realvnc-vnc-viewer 
+  chromium realvnc-vnc-server realvnc-vnc-viewer cockpit
 
   

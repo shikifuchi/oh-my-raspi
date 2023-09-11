ec70389c338957f6192fbdb5a595dd3e1754c4ef
Benedict <shikifuchi@hotmail.com>
2023-09-11 16:30:02 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo apt update --yes
-sudo apt install --yes build-essential git vim neovim python3 python3-venv python3-pip nodejs\
+sudo apt install --yes build-essential git vim neovim python3 python3-venv python3-pip nodejs npm\
   lightdm slick-greeter i3 polybar picom rofi suckless-tools\
   chromium realvnc-vnc-server realvnc-vnc-viewer cockpit\
-  gzip
-
+  gzip meson ninja-build
   

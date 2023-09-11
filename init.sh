dd6dfbf4eaedb2d36804eb8b33571644635d1738
Benedict <shikifuchi@hotmail.com>
2023-09-11 19:28:36 +0800

    adjustments

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update --yes
 sudo apt install --yes build-essential git vim neovim python3 python3-venv python3-pip nodejs npm\
-  lightdm slick-greeter i3 polybar picom rofi suckless-tools\
+  lightdm slick-greeter i3 polybar picom rofi suckless-tools cmake\
   chromium tightvncserver cockpit\
   gzip meson ninja-build xfce4-terminal
   

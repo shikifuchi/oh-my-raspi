dede00dee90d1da19743ccbdb0f8fd5da2a4f732
Benedict <shikifuchi@hotmail.com>
2023-09-11 21:54:04 +0800

    update clang

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 sudo apt update --yes
 sudo apt install --yes build-essential git vim neovim python3 python3-venv python3-pip nodejs npm\
   lightdm slick-greeter i3 polybar picom rofi suckless-tools cmake\
-  chromium tightvncserver cockpit\
+  chromium tightvncserver cockpit clang clangd\
   gzip meson ninja-build xfce4-terminal
   

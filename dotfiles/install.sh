00c245ff9261fe7572b3d1e36cfdf2520d1c9271
Benedict <shikifuchi@hotmail.com>
2023-09-11 13:06:25 +0800

    fix link

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -3,7 +3,9 @@
 rm -rf ~/.bashrc
 rm -rf ~/.zshrc
 rm -rf ~/.custom_sh_rc
+rm -rf ~/.vimrc
 
 ln -s $PWD/bashrc ~/.bashrc
 ln -s $PWD/zshrc ~/.zshrc
-ln -s $PWD/custom_sh_rc ~/.cutom_sh_rc
+ln -s $PWD/custom_sh_rc ~/.custom_sh_rc
+ln -s $PWD/vimrc ~/.vimrc

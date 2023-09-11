47ef41d6b764085e5488cff0bacc5074c2dd65bc
Benedict <shikifuchi@hotmail.com>
2023-09-11 16:41:42 +0800

    update config

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo apt update --yes
 sudo apt install --yes build-essential git vim neovim python3 python3-venv python3-pip nodejs npm\
   lightdm slick-greeter i3 polybar picom rofi suckless-tools\
   chromium realvnc-vnc-server realvnc-vnc-viewer cockpit\
-  gzip meson ninja-build
+  gzip meson ninja-build xfce4-terminal
   

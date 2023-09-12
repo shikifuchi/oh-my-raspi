4975ce955dd118aae79155656d2b5f2f0b170801
Benedict <shikifuchi@hotmail.com>
2023-09-12 23:06:27 +0800

    add network applet

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update --yes
-sudo apt install --yes build-essential git vim neovim python3 python3-venv python3-pip nodejs npm\
+sudo apt full-upgrade --yes
+sudo apt install --yes build-essential git vim python3 python3-venv python3-pip nodejs npm\
   lightdm slick-greeter i3 polybar picom rofi suckless-tools cmake\
   chromium tightvncserver cockpit clang clangd\
-  gzip meson ninja-build xfce4-terminal
-  
+  gzip meson ninja-build xfce4-terminal network-manager-gnome

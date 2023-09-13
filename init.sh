27a54f59e19bcb861ea993c0bbf8c4ddb0b7ab42
Benedict <shikifuchi@hotmail.com>
2023-09-13 23:29:12 +0800

    update script

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 sudo apt update --yes
 sudo apt full-upgrade --yes
 sudo apt install --yes build-essential git vim python3 python3-venv python3-pip nodejs npm\
-  lightdm slick-greeter i3 polybar picom rofi suckless-tools cmake\
+  lightdm slick-greeter i3 polybar picom rofi suckless-tools cmake nginx\
   chromium tightvncserver cockpit clang clangd\
   gzip meson ninja-build xfce4-terminal network-manager-gnome
+sudo apt autoremove
